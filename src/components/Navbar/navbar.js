50b26a13049d54bc5f0dc86b4e66996bdeef74f5
MercyChebet <chebetm96@gmail.com>
2024-02-27 15:53:46 +0300

    Changes

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+import logo from "../../assets.logo.npg"
+
+const Navbar = () => {
+  return (
+    <nav classname="navbar">
+      <img src="" alt="logo" />
+      <div classname="desktopMenu">
+
+      </div>
+      <button classname="desktopMenuBtn">
+        <img src="" alt="contactmeimage" classname="desktopMenuImg"/>Contact Me </button>
+
+    </nav>
+  )
+}
+
+export default Navbar
